5f7baa48fa21628664356d2b79d64254be770ff6
ayatu <806149557@qq.com>
2020-12-02 20:47:21 +0800

    补充fetch插件，添加post请求

--- src/plugins/fetch.js
+++ src/plugins/fetch.js
@@ -10,18 +10,20 @@ export default {
 
 async function $fetch(url, options) {
     const finalOptions = Object.assign({}, {
-
-    })
+        headers: {
+            'Content-Type': 'application/json'   //告诉服务器我们的请求主体时Json
+        },
+        credentials: 'include'    //请求时携带cookie
+    }, options)
 
     const response = await fetch(baseUrl + url, finalOptions)
     if (response.ok) {
         const data = await response.json()
         return data
     } else {
-        const error = new Error('error')
+        const message = await response.text()  //获取服务器返回的错误信息(一般会返回文本)
+        const error = new Error(message)
+        error.response = response
         throw error
     }
-
-
-
 }
